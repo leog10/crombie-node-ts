713c570045256e71752fa4d514fd01a6964fdd81
Leo <leonardogilli@outlook.com>
2022-11-20 17:45:55 -0300

    Fixed missing database PORT variable

--- config/config.ts
+++ config/config.ts
@@ -7,5 +7,5 @@ export const DB_PASSWORD = process.env.DB_PASSWORD || '';
 export const DB_NAME = process.env.DB_NAME || 'product_db';
 export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_DIALECT = 'mysql';
-export const DB_PORT = process.env.DB_PORT || '3306';
+export const DB_PORT = Number(process.env.DB_PORT) || 3306;
 export const PORT = process.env.PORT || 5000;
--- models/index.ts
+++ models/index.ts
@@ -4,12 +4,14 @@ import {
   DB_HOST,
   DB_NAME,
   DB_PASSWORD,
+  DB_PORT,
   DB_USER,
 } from '../config/config';
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: DB_DIALECT,
+  port: DB_PORT,
 });
 
 export default sequelize;
